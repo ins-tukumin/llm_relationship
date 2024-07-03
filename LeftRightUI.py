74e5c113fd61b1b2f12b95eaf48a3a601402a617
ins-tukumin <116026504+ins-tukumin@users.noreply.github.com>
2024-07-04 01:25:12 +0900

    Update LeftRightUI.py

--- LeftRightUI.py
+++ LeftRightUI.py
@@ -56,6 +56,40 @@ prompt = ChatPromptTemplate.from_messages([
         #llm=llm)
     #return conversation
 model_select = "gpt-4o"
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 #st.write(model_select)
 # デコレータを使わない会話履歴読み込み for セッション管理
 def load_conversation():
