5ab34ce904a1687a57e29410cf95748235481e30
ins-tukumin <116026504+ins-tukumin@users.noreply.github.com>
2024-07-25 14:38:45 +0900

    Update LeftRightUI.py

--- LeftRightUI.py
+++ LeftRightUI.py
@@ -32,7 +32,7 @@ if "past" not in st.session_state:
 if "initialized" not in st.session_state:
     st.session_state['initialized'] = False
     initial_message = "こんにちは、素晴らしい会話をしよう！"
-    st.session_state.generated.append(initial_message)
+    st.session_state.initge.append(initial_message)
     #st.session_state.past.append("")
     message(st.session_state.initge[0], key="init_greeting", avatar_style="micah")
     st.session_state['initialized'] = True
